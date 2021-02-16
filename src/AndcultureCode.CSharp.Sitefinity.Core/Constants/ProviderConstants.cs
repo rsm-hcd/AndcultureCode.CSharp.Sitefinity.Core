a3af361c2e7ea2e5c9102985f1b1f8bc28db20ad
Cody Spath <cspath@andculture.com>
2021-02-16 16:36:09 -0500

    Add ProviderConstants

---
+++ src/AndcultureCode.CSharp.Sitefinity.Core/Constants/ProviderConstants.cs
@@ -0,0 +1,8 @@
+﻿
+namespace AndcultureCode.CSharp.Sitefinity.Core.Constants
+{
+    public static class ProviderConstants
+    {
+        public static string OpenAccessProvider = "OpenAccessProvider";
+    }
+}
