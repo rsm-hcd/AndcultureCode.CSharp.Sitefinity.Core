c4f30f42b34f7967b0efb941e1ddbb8189e5f616
Cody Spath <cspath@andculture.com>
2021-02-16 10:53:44 -0500

    Add SitefinityMetadataAttribute

---
+++ src/AndcultureCode.CSharp.Sitefinity.Core/Attributes/SitefinityMetadataAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AndcultureCode.CSharp.Sitefinity.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SitefinityMetadataAttribute : Attribute
+    {
+        public string DynamicContentType { get; }
+        public string ParentDynamicContentType { get; }
+
+        public SitefinityMetadataAttribute(string dynamicContentType, string parentDynamicContentType = null)
+        {
+            DynamicContentType = dynamicContentType;
+            ParentDynamicContentType = parentDynamicContentType;
+        }
+    }
+}
