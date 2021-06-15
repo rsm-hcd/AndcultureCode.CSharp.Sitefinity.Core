04c46fbba648cfefd98136e3d2d806774969e297
Kevin Busch <kbusch@andculture.com>
2021-06-15 19:30:55 -0400

    Added the child sitefinity content object to support parent associations through test suite code

---
+++ src/AndcultureCode.CSharp.Sitefinity.Core/Models/Content/ChildSitefinityContent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AndcultureCode.CSharp.Sitefinity.Core.Models.Content
+{
+    public class ChildSitefinityContent<T> : SitefinityContent
+        where T : SitefinityContent
+    {
+        public Guid SystemParentId { get; set; }
+        public T SystemParentItem { get; set; }
+    }
+}
